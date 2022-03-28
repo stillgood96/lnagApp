da2b1974833e756465cf5fb32868da6380a1e8cb
imlsw96 <a787574@naver.com>
2022-03-29 01:34:35 +0900

    #4.3 The Rules of Animation (07:34)

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Animated } from 'react-native';
 import styled from 'styled-components/native';
 
 const Container = styled.View`
@@ -12,22 +13,15 @@ const Box = styled.TouchableOpacity`
   height: 200px;
 `;
 
+const AnimatedBox = Animated.createAnimatedComponent(Box);
 
 export default function App() {
 
-  const [y , setY] = useState(0);
-  const [intervalId , setIntervalId] = useState(null);
+  const y = new Animated.Value(0);
   const moveUp = ()=> {
-    const id = setInterval(() => {setY((prev) => prev+1)}, 10)
-    setIntervalId(id);
   }
 
-  useEffect(()=> {
-    if(y === 200) {
-      clearInterval(intervalId);
-    }
-    
-  }, [y,intervalId]);
+
   return <Container>
     <Box onPress={moveUp} style={{
       transform: [{translateY:y}]
