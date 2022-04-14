e4869d6dee50c5a7d095b1e816ded1dfe22a830b
stillgood96 <a787574@naver.com>
2022-04-15 05:44:30 +0900

    #4.7 More Interpolations (09:08)

--- App.js
+++ App.js
@@ -18,35 +18,40 @@ const AnimatedBox = Animated.createAnimatedComponent(Box);
 export default function App() {
 
   const [up, setUp] = useState(false);
-
-  const Y_POSITION = useRef(new Animated.Value(300)).current;
+  const POSITION = useRef(new Animated.ValueXY({x:0, y:300})).current;
 
   const toggleUp = () => setUp(prev => !prev);
   const moveUp = ()=> {
-    Animated.timing(Y_POSITION, {
+    Animated.timing(POSITION, {
       toValue : up? 300 :  -300,
-      useNativeDriver : true,
+      useNativeDriver : false,
       duration: 5000
     }).start(toggleUp);
   }
 
-  const opacityValue = Y_POSITION.interpolate({
 
-    inputRange: [-300, -100, 100, 300],
-    outputRange: [1, 0.1,0.1, 1]
+  const rotation = POSITION.y.interpolate({
+    inputRange : [-300, 300],
+    outputRange: ["-360deg", "360deg"],
   });
 
-  const borderRadius = Y_POSITION.interpolate({
+  const borderRadius = POSITION.y.interpolate({
     inputRange : [-300, 300],
     outputRange : [100, 0]
   })
 
+  const bgColor = POSITION.y.interpolate( {
+    inputRange : [-300, 300],
+    outputRange : ["rgb(255, 99, 71)", "rgb(71, 166, 255)"]
+  })
+
+  POSITION.addListener(() => console.log(rotation));
   return <Container>
     <Pressable onPress={moveUp}>
     <AnimatedBox  style={{
-      opacity : opacityValue,
       borderRadius,
-      transform: [{translateY:Y_POSITION}]
+      transform: [{rotateY : rotation},{translateY:POSITION.y}],
+      backgroundColor: bgColor
     }}></AnimatedBox>
     </Pressable>
   
